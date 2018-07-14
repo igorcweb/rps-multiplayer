be972e64bc85b0d7d75a9c149b1a45c3605e1997
Igor Chernyshev <igorchern07@gmail.com>
2018-07-13 22:02:13 -0500

    firebase starter

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,5 +12,67 @@
 
   const database = firebase.database();
 
-  console.log(database);
+  const join = $('.join');
+  const nameInput = $('#inputName');
+  const joinForm = $('form.join');
+
+  let data;
+  let numPlayers;
+  let game;
+
+  database.ref().on('value', function(snap) {
+    data = snap.val();
+    //console.log(data);
+    numPlayers = 0;
+    //127.0.0.1:5500/index.html?
+    http: if (data) {
+      for (player in data.players) {
+        numPlayers++;
+        // console.log(player);
+        // console.log(numPlayers);
+      }
+    }
+  });
+
+  join.on('click', function(e) {
+    e.preventDefault();
+    player = nameInput.val().trim();
+    // console.log(player);
+    // console.log('numFromButton: ', numPlayers);
+    nameInput.val('');
+    if (player) {
+      // if (numPlayers < 2) {
+      //   database.ref('/players').push({
+      //     name: player,
+      //     wins: 0,
+      //     losses: 0
+      //   });
+      //   joinForm.addClass('d-none');
+      // } else {
+      //   console.log(
+      //     'Two players are currently playing.  Please wait when someone leaves.'
+      //   );
+      // }
+      numPlayers = 1;
+      if (numPlayers % 2 === 0) {
+        database.ref().push({
+          game: {
+            players: {
+              name: player,
+              wins: 0,
+              losses: 0
+            }
+          }
+        });
+        joinForm.addClass('d-none');
+        console.log(data);
+      } else {
+        database.ref('/game/players').push({
+          name: player,
+          wins: 0,
+          losses: 0
+        });
+      }
+    }
+  });
 })();
